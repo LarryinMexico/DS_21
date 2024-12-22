984f763e3586a8e6a6a609355b10f8d0bf4affa1
LarryinMexico <sam92.chen@gmail.com>
2024-12-22 14:39:38 +0800

    good

--- src/main/java/com/finalproject/FinalProject/GoogleSearchController.java
+++ src/main/java/com/finalproject/FinalProject/GoogleSearchController.java
@@ -14,18 +14,24 @@ import java.util.*;
 @RestController
 @RequestMapping("/api")
 public class GoogleSearchController {
-
     @Autowired
-    private GoogleQueryService searchService;
+    private CrawlerService webCrawlerService;
+    
+    @Autowired
+    private MovieProcessingService movieProcessingService;
+    
+    @Autowired
+    private WikiService wikipediaService;
 
     @GetMapping("/movies/summary")
     public ResponseEntity<String> getMovieSummary(@RequestParam String movieName) {
         try {
-            String summary = searchService.fetchWikipediaSummary(movieName);
+            String summary = wikipediaService.fetchWikipediaSummary(movieName);
             return ResponseEntity.ok(summary);
         } catch (IOException e) {
             e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("發生錯誤，無法獲取簡介");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("發生錯誤，無法獲取簡介");
         }
     }
     
@@ -33,22 +39,22 @@ public class GoogleSearchController {
     public ResponseEntity<Map<String, Object>> searchMovies(@RequestParam String keyword) {
         try {
             // 1.Google 搜尋，獲取前5個網站URL
-            List<String> websites = searchService.query(keyword);
+            List<String> websites = webCrawlerService.query(keyword);
 
             // 2.爬取每個網站的h2和h3標籤內容
-            List<String> websiteTexts = searchService.fetchFromWebsites(websites);
+            List<String> websiteTexts = webCrawlerService.fetchFromWebsites(websites);
 
             // 3.提取電影名稱
-            List<String> movieNames = searchService.extractMovieNames(websiteTexts);
+            List<String> movieNames = movieProcessingService.extractMovieNames(websiteTexts);
 
             // 4.計算電影名稱出現次數
-            Map<String, Integer> movieScores = searchService.calculateMovieScore(movieNames);
+            Map<String, Integer> movieScores = movieProcessingService.calculateMovieScore(movieNames);
             
             // 5.清理搜尋結果
-            Map<String, Integer> filteredScores = searchService.filterResults(movieScores);
+            Map<String, Integer> filteredScores = movieProcessingService.filterResults(movieScores);
             
             // 6.其他人也搜尋了: 抓不到
-            List<String> relatedSearches = searchService.fetchRelatedSearches(keyword);
+            List<String> relatedSearches = webCrawlerService.fetchRelatedSearches(keyword);
             
             
             Map<String, Object> response = new HashMap<>();
