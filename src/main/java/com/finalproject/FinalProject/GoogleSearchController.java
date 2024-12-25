a5e02590fc6aecfe4cb8345edfe69b3ec485f10e
LarryinMexico <sam92.chen@gmail.com>
2024-12-25 17:15:00 +0800

    update related search and general search

--- src/main/java/com/finalproject/FinalProject/GoogleSearchController.java
+++ src/main/java/com/finalproject/FinalProject/GoogleSearchController.java
@@ -23,39 +23,47 @@ public class GoogleSearchController {
     @Autowired
     private WikiService wikipediaService;
     
+    // 進行電影搜索或一般搜索
     @GetMapping("/movies/search")
     public ResponseEntity<Map<String, Object>> searchMovies(@RequestParam String keyword) {
         try {
-            // 1.Google搜尋，獲得前5個網站URL
-            List<String> websites = crawlerService.query(keyword);
-
-            // 2.爬取每個網站的h2和h3標籤內容作為電影名稱來源
-            List<String> websiteTexts = crawlerService.fetchFromWebsites(websites);
-
-            // 3.將提取到的電影名稱作處理
-            List<String> movieNames = movieProcessingService.extractMovieNames(websiteTexts);
-
-            // 4.計算電影名稱出現次數
-            Map<String, Integer> movieScores = movieProcessingService.calculateMovieScore(movieNames);
-            
-            // 5.清理搜尋結果
-            Map<String, Integer> filteredScores = movieProcessingService.filterResults(movieScores);
-            
-            // 6.其他人也搜尋了（抓不到）
+        	Map<String, Object> response = new HashMap<>();
+            boolean isMovieCategory = crawlerService.isMovieCategory(keyword);
             List<String> relatedSearches = crawlerService.fetchRelatedSearches(keyword);
             
-            // 將結果Map回傳
-            Map<String, Object> response = new HashMap<>();
-            response.put("scores", filteredScores);
-            response.put("relatedSearches", relatedSearches);
+            if (isMovieCategory) { // 電影搜索
+            	
+                // 1.Google搜尋，獲得前5個網站URL
+                List<String> websites = crawlerService.Moviequery(keyword);
+                // 2.爬取每個網站的h2和h3標籤內容作為電影名稱來源
+                List<String> websiteTexts = crawlerService.fetchFromWebsites(websites);
+                // 3.將提取到的電影名稱作處理
+                List<String> movieNames = movieProcessingService.extractMovieNames(websiteTexts);
+                // 4.計算電影名稱出現次數
+                Map<String, Integer> movieScores = movieProcessingService.calculateMovieScore(movieNames);
+                // 5.清理搜尋結果
+                Map<String, Integer> filteredScores = movieProcessingService.filterResults(movieScores);
+                
+                response.put("scores", filteredScores);
+                response.put("isMovieCategory", true);
+            } else { // 一般搜索
+            	
+                List<String> searchResults = crawlerService.generalSearch(keyword);
+                response.put("generalResults", searchResults);
+                response.put("isMovieCategory", false);
+            }
             
+            // 將結果Map回傳
+            response.put("relatedSearches", relatedSearches);
             return ResponseEntity.ok(response);
+            
         } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Collections.emptyMap());
         }
     }
-    
+
+    // 抓取電影簡介
     @GetMapping("/movies/summary")
     public ResponseEntity<String> getMovieSummary(@RequestParam String movieName) {
         try {
